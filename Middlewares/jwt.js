dfa5f58c05b01bd33ee93061d79adc23b81c2de9
Omar Albaz <omaralbaz635@gmail.com>
2025-07-28 14:46:27 +0300

    update verify_token

--- Middlewares/jwt.js
+++ Middlewares/jwt.js
@@ -9,27 +9,23 @@ export const generate_token = (email, id, role) => {
 export const verify_token = (req, res, next) => {
   const Bearer_token = req.headers.authorization;
   const token = Bearer_token.split(" ")[1];
+  if (!token || token == undefined || token == null) {
+    return res
+      .status(401)
+      .json({ status: "Failed", error: "token is required , login to go" });
+  }
+  const current_user = JsonWebToken.verify(token, process.env.secret_key);
+  if (!current_user) {
+    return res
+      .status(401)
+      .json({ status: "Failed", error: "invalid token or expired" });
+  }
   try {
-    if (!token || token == undefined || token == null) {
-      return res
-        .status(401)
-        .json({ status: "Failed", error: "token is required , login to go" });
-    }
-    const current_user = JsonWebToken.verify(token, process.env.secret_key);
     req.current_user = current_user;
     next();
   } catch (err) {
-    if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
-      return res.status(401).json({
-        status: "Failed",
-        error:
-          err.name === "TokenExpiredError"
-            ? "Token has expired. Please login again"
-            : "Invalid token",
-      });
-    }
     return res.status(500).json({
-      status: "Error",
+      status: "error",
       error: err.message,
     });
   }
